0d4cacdcd9314debf5bf3337a589ca4da71ae0c2
PNG49364936 <pierrenoelgauthier@MacBook-Air-de-PIERRE.local>
2026-01-18 08:29:20 +0100

    git

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -38,8 +38,46 @@ class PredictionsController < ApplicationController
 
     # Prédiction
     @prix_prevu = model.predict(Numo::DFloat[[ @taille ]])[0]
+
+    # Majoration de 10% si bien neuf
+    @type_bien = params[:type_bien] || "ancien"
+    if @type_bien == "neuf"
+      @prix_prevu = @prix_prevu * 1.10
+    end
+
+    # Ajustement selon l'état du bien
+    @etat_bien = params[:etat_bien] || "bon_etat"
+    @ajustement_etat = nil
+    if @type_bien == "ancien"
+      case @etat_bien
+      when "a_renover"
+        @prix_prevu = @prix_prevu * 0.85
+        @ajustement_etat = "-15%"
+      when "bon_etat"
+        @prix_prevu = @prix_prevu * 0.95
+        @ajustement_etat = "-5%"
+      when "totalement_renove"
+        @prix_prevu = @prix_prevu * 1.15
+        @ajustement_etat = "+15%"
+      end
+    elsif @type_bien == "neuf"
+      case @etat_bien
+      when "a_renover"
+        @prix_prevu = @prix_prevu * 0.90
+        @ajustement_etat = "-10%"
+      when "bon_etat"
+        # Pas de modification
+        @ajustement_etat = nil
+      when "totalement_renove"
+        @prix_prevu = @prix_prevu * 1.10
+        @ajustement_etat = "+10%"
+      end
+    end
+
     @resultat = "Prix estimé : #{@prix_prevu.round(2)} €"
-    puts "DEBUG >> @taille = #{@taille}, @prix_prevu = #{@prix_prevu}"
+    @resultat += " (neuf +10%)" if @type_bien == "neuf"
+    @resultat += " (#{@etat_bien.gsub('_', ' ')} #{@ajustement_etat})" if @ajustement_etat
+    puts "DEBUG >> @taille = #{@taille}, @prix_prevu = #{@prix_prevu}, @type_bien = #{@type_bien}"
 
     render :predict
   end
